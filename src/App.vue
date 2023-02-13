c93fb4223fa251aa581ee2990aef71037e5305a5
Komgrit015 <ford_za007@hotmail.com>
2023-02-13 11:47:34 +0700

    Get api openWeathermap

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
 <template>
-  <div id="main">
+  <div id="main" :class="isDay ? 'day':'night'">
   <div class="container my-5">
     <h1 class="title text-center">Weather in</h1>
-    <form class="search-location">
+    <form class="search-location" v-on:submit.prevent="getWeather">
       <input
         type="text"
         class="form-control text-muted form-rounded p-4 shadow-sm"
         placeholder="What City?"
+        v-model="citySearch"
         autocomplete="off"
       />
     </form>
-    <div class="card form-rounded my-3 shadow-lg back-card overflow-hidden">
+    <p class="text-center my-3" v-if="cityFound">No city found</p>
+    <div class="card form-rounded my-3 shadow-lg back-card overflow-hidden" v-if="visible">
+
+        <!-- weather animation container -->
+        <div>
+          <div icon="sunny" v-if="clearSky" data-label="Sunny">
+            <span class="sun"></span>
+          </div>
+
+          <div icon="snowy" v-if="snowy" data-label="Chilly">
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+
+          <div icon="stormy" v-if="stormy" data-label="Soggy">
+            <span class="cloud"></span>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div icon="cloudy" v-if="cloudy" data-label="Perfect">
+            <span class="cloud"></span>
+            <span class="cloud"></span>
+          </div>
+          <div icon="nightmoon" v-if="clearNight" data-label="Cool!">
+            <span class="moon"></span>
+            <span class="meteor"></span>
+          </div>
+        </div>
+
       <div class="card-top text-center" style="margin-bottom: 15rem">
       <div class="city-name my-3">
-        <p>Phuket</p>
+        <p>{{ weather.cityName }}</p>
         <span>...</span>
-        <p class="">TH</p>
+        <p class="">{{ weather.country }}</p>
       </div>
     </div>
    
@@ -25,19 +72,19 @@
       <div class="card-mid">
         <div class="row">
           <div class="col-12 text-center temp">
-            <span>20&deg;C</span>
-            <p class="my-4">Clouds everywhere</p>
+            <span>{{ weather.temperature }}&deg;C</span>
+            <p class="my-4">{{ weather.description }}</p>
           </div>
         </div>
         <div class="row">
           <div class="col d-flex justify-content-between px-5 mx-5">
             <p>
               <img src="./assets/down.svg" alt="" />
-              18&deg;C
+              {{ weather.lowTemp }}&deg;C
             </p>
             <p>
               <img src="./assets/up.svg" alt="" />
-              25&deg;C
+              {{ weather.highTemp }}&deg;C
             </p>
           </div>
         </div>
@@ -45,11 +92,11 @@
       <!-- card bottom starts here -->
       <div class="card-bottom px-5 py-4 row">
         <div class="col text-center">
-          <p>20&deg;C</p>
+          <p>{{ weather.feelsLike }}&deg;C</p>
           <span>Feels like</span>
         </div>
         <div class="col text-center">
-          <p>55%</p>
+          <p>{{ weather.humidity }}%</p>
           <span>humidity</span>
         </div>
       </div>
@@ -61,7 +108,115 @@
 
 <script>
 export default {
-  name: "App",
+  data(){
+    return{
+      cityFound: false,
+      visible: false,
+      stormy: false,
+      cloudy: false,
+      clearSky: false,
+      clearNight: false,
+      snowy: false,
+
+      isDay: true,
+      citySearch: "",
+      weather:{
+        cityName: "Phuket",
+        country: "TH",
+        temperature: 30,
+        description: "Clouds everywhere",
+        lowTemp: "28",
+        highTemp: "34",
+        feelsLike: "30",
+        humidity: "70",
+      },
+    };
+  },
+  methods:{
+    getWeather: async function(){
+      console.log(this.citySearch);
+      const key = "d8201d46fcc0a551e794af0f292c4252"
+      const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${this.citySearch}&appid=${key}&units=metric`;
+
+      try {
+      const response = await fetch(baseURL);
+      const data = await response.json();
+      console.log(data);
+      this.citySearch = "";
+      this.weather.cityName = data.name;
+      this.weather.country = data.sys.country;
+      this.weather.temperature = Math.round(data.main.temp);
+      this.weather.description = data.weather[0].description;
+      this.weather.lowTemp = Math.round(data.main.temp_min);
+      this.weather.highTemp = Math.round(data.main.temp_max);
+      this.weather.feelsLike = Math.round(data.main.feels_like);
+      this.weather.humidity = Math.round(data.main.humidity);
+
+      const timeOfDay = data.weather[0].icon;
+        ///check for time of day
+        if (timeOfDay.includes("n")) {
+          this.isDay = false;
+        } else {
+          this.isDay = true;
+        }
+
+        const mainWeather = data.weather[0].main;
+        //check weather animations
+
+        if(mainWeather.includes("Clouds")){
+          this.stormy= false;
+          this.cloudy= true;
+          this.clearSky= false;
+          this.clearNight= false;
+          this.snowy= false;
+        }
+        if(mainWeather.includes("Clouds")){
+          this.stormy= false;
+          this.cloudy= true;
+          this.clearSky= false;
+          this.clearNight= false;
+          this.snowy= false;
+        }
+        if(mainWeather.includes("Thunderstorm")||
+          mainWeather.includes("Rain")){
+          this.stormy= true;
+          this.cloudy= false;
+          this.clearSky= false;
+          this.clearNight= false;
+          this.snowy= false;
+        }
+        if (mainWeather.includes("Clear") && this.isDay){
+          this.stormy= false;
+          this.cloudy= false;
+          this.clearSky= true;
+          this.clearNight= false;
+          this.snowy= false;
+        }
+        if (mainWeather.includes("Clouds") && !this.isDay){
+          this.stormy= false;
+          this.cloudy= false;
+          this.clearSky= false;
+          this.clearNight= true;
+          this.snowy= false;
+        }
+        if (mainWeather.includes("Snow")){
+          this.stormy= false;
+          this.cloudy= false;
+          this.clearSky= false;
+          this.clearNight= false;
+          this.snowy= true;
+        }
+
+        this.visible = true;
+        this.cityFound = false;
+      } catch (error) {
+        console.log(error);
+        this.cityFound = true;
+        this.visible = false;
+      }
+      
+    }
+  }
 };
 </script>
 
